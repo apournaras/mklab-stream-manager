e05deb6ea5a6dcee016a974b081a0da51c513359
manosetro <manosetro@iti.gr>
2013-11-20 11:28:28 +0200

    Change SolrStorage to index original items

--- src/main/java/eu/socialsensor/sfc/streams/store/SolrStorage.java
+++ src/main/java/eu/socialsensor/sfc/streams/store/SolrStorage.java
@@ -7,15 +7,13 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
 
 import eu.socialsensor.framework.client.dao.ItemDAO;
 import eu.socialsensor.framework.client.dao.MediaItemDAO;
 import eu.socialsensor.framework.client.dao.impl.ItemDAOImpl;
 import eu.socialsensor.framework.client.dao.impl.MediaItemDAOImpl;
+import eu.socialsensor.framework.client.search.solr.SolrItemHandler;
 import eu.socialsensor.framework.client.search.solr.SolrMediaItemHandler;
-import eu.socialsensor.framework.client.search.solr.SolrTopicDetectionItem;
 import eu.socialsensor.framework.common.domain.Item;
 import eu.socialsensor.framework.common.domain.MediaItem;
 import eu.socialsensor.sfc.streams.StorageConfiguration;
@@ -32,21 +30,21 @@ public class SolrStorage implements StreamUpdateStorage {
 	
 	private static final String HOSTNAME = "solr.hostname";
 	private static final String SERVICE = "solr.service";
-	private static final String TOPICDETECTIONITEMS_COLLECTION = "solr.topicDetectionItems.collection";
+	private static final String ITEMS_COLLECTION = "solr.items.collection";
 	private static final String MEDIAITEMS_COLLECTION = "solr.mediaitems.collection";
 	
 	private String hostname, service;
-	private String topicDetectionItemsCollection = null;
+	private String itemsCollection = null;
 	private String mediaItemsCollection = null;
 	
-	private HttpSolrServer topicDetectionItemsServer = null;
+	private SolrItemHandler solrItemHandler = null; 
 	private SolrMediaItemHandler solrMediaHandler = null; 
 	
 	public SolrStorage(StorageConfiguration config) throws IOException {
 		this.hostname = config.getParameter(SolrStorage.HOSTNAME);
 		this.service = config.getParameter(SolrStorage.SERVICE);
+		this.itemsCollection = config.getParameter(SolrStorage.ITEMS_COLLECTION);
 		this.mediaItemsCollection = config.getParameter(SolrStorage.MEDIAITEMS_COLLECTION);
-		this.topicDetectionItemsCollection = config.getParameter(SolrStorage.TOPICDETECTIONITEMS_COLLECTION);
 	}
 	
 	public SolrMediaItemHandler getHandler(){
@@ -55,8 +53,8 @@ public class SolrStorage implements StreamUpdateStorage {
 	
 	@Override
 	public void open() throws IOException {
-		if(topicDetectionItemsCollection != null) {
-			topicDetectionItemsServer = new HttpSolrServer(hostname+"/"+service+"/"+topicDetectionItemsCollection);
+		if(itemsCollection != null) {	
+			solrItemHandler = SolrItemHandler.getInstance(hostname+"/"+service+"/"+itemsCollection);
 		}
 		if(mediaItemsCollection != null) {	
 			solrMediaHandler = SolrMediaItemHandler.getInstance(hostname+"/"+service+"/"+mediaItemsCollection);
@@ -65,45 +63,32 @@ public class SolrStorage implements StreamUpdateStorage {
 
 	@Override
 	public void store(Item item) throws IOException {
-		SolrTopicDetectionItem solrTopicDetectionItem = new SolrTopicDetectionItem(item);
-		try {
-			if(topicDetectionItemsServer != null) {
-				topicDetectionItemsServer.addBean(solrTopicDetectionItem);	
-			}
+		if(solrItemHandler != null) {
+			solrItemHandler.insertItem(item, null);
+		}
+		if(solrMediaHandler != null) {
 			
-			if(solrMediaHandler != null) {
+			for(Entry<URL, MediaItem> entry : item.getMediaItems().entrySet()) {
 				
-				for(Entry<URL, MediaItem> entry : item.getMediaItems().entrySet()) {
-					
-					MediaItem mediaItem = new MediaItem(entry.getKey(), item);
-					//System.out.println(mediaItem.toJSONString());
-					
-					MediaItem mi = solrMediaHandler.getSolrMediaItem(mediaItem.getId());
-					if(mi==null) {
-						//System.out.println("New media item");
-						solrMediaHandler.insertMediaItem(mediaItem, null);
-					}
-					else {
-						//System.out.println("Media item "+mi.getId()+" already exists - update");
-				
-						
-						for(String key : mediaItem.getFeedKeywords()){
-							if(!mi.getFeedKeywords().contains(key)){
-								//System.out.println("Add keyword : "+key);
-								mi.getFeedKeywords().add(key);
-							}
-								
+				MediaItem mediaItem = new MediaItem(entry.getKey(), item);	
+				MediaItem mi = solrMediaHandler.getSolrMediaItem(mediaItem.getId());
+				if(mi==null) {
+					solrMediaHandler.insertMediaItem(mediaItem, null);
+				}
+				else {
+					for(String key : mediaItem.getFeedKeywords()){
+						if(!mi.getFeedKeywords().contains(key)){
+							mi.getFeedKeywords().add(key);
 						}
-						for(String key : mediaItem.getFeedKeywordsString()) {
-							if(!mi.getFeedKeywordsString().contains(key))
-								mi.getFeedKeywordsString().add(key);
-						}
-						solrMediaHandler.insertMediaItem(mi, null);
+							
 					}
+					for(String key : mediaItem.getFeedKeywordsString()) {
+						if(!mi.getFeedKeywordsString().contains(key))
+							mi.getFeedKeywordsString().add(key);
+					}
+					solrMediaHandler.insertMediaItem(mi, null);
 				}
 			}
-		} catch (SolrServerException e) {
-			throw new IOException(e);
 		}
 		
 	}
@@ -117,13 +102,7 @@ public class SolrStorage implements StreamUpdateStorage {
 	@Override
 	public boolean delete(String itemId) throws IOException {
 		logger.info("Delete item with id " + itemId + " from Solr.");
-		try {
-			if(topicDetectionItemsServer != null) {
-				topicDetectionItemsServer.deleteById(itemId);
-			}
-		} catch (SolrServerException e) {
-			return false;
-		}
+		solrItemHandler.deleteItem(itemId);
 		return true;
 	}
 
@@ -146,12 +125,7 @@ public class SolrStorage implements StreamUpdateStorage {
 	}
 	
 	private void commit() throws IOException {
-		try {
-			topicDetectionItemsServer.optimize();
-			topicDetectionItemsServer.commit();
-		} catch (SolrServerException e) {
-			
-		}
+	
 	}
 	
 	public static void main(String[] args) throws IOException {
