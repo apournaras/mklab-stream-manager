1bf531506587cfdb0f03ec879fb6258d347ad298
matzika <katerina.iliakopoulou@gmail.com>
2013-12-04 16:34:13 +0200

    Add subscriber

---
+++ src/main/java/eu/socialsensor/sfc/streams/SubscriberConfiguration.java
@@ -0,0 +1,14 @@
+package eu.socialsensor.sfc.streams;
+
+import eu.socialsensor.framework.streams.StreamHandler;
+
+public class SubscriberConfiguration extends Configuration{
+	public static final String CLASS_PATH = "Classpath";
+	
+	private StreamHandler handler;
+	
+	public StreamHandler getHandler() {
+		// TODO Auto-generated method stub
+		return handler;
+	}
+}
