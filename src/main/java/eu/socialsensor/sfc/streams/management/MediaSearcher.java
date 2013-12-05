ef1ea5619bc1973f118eb5dd6614a7531f5bd28a
matzika <katerina.iliakopoulou@gmail.com>
2013-12-05 15:16:54 +0200

    MediaSearcher class added

---
+++ src/main/java/eu/socialsensor/sfc/streams/management/MediaSearcher.java
@@ -0,0 +1,13 @@
+package eu.socialsensor.sfc.streams.management;
+
+public class MediaSearcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
